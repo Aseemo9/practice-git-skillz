a5ad52c8f415f0e325e0561ff3338529d3dacc6b
Aseem <aseem.sharma@o9solutions.com>
2023-05-11 14:17:24 +0530

    Changes done in sql from git bash

--- test.sql
+++ test.sql
@@ -4,5 +4,5 @@ INSERT INTO mytargetdatabase_test.my_test_table
 SELECT * FROM mysourcedatabase_test.my_test_table a
 INNER JOIN mysourcedatabase_test.another_test_table b
             ON a.columnA = b.columnA
-WHERE columnA IN ('This Condition','This Filter')
+WHERE columnA = 'This Condition'
 ;
